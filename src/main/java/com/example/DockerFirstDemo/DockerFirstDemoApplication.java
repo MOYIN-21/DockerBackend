1d53b575ae3747f597eab71db7dd68bd9476ecc2
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-04-05 14:46:04 +0100

    first commit

---
+++ src/main/java/com/example/DockerFirstDemo/DockerFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerFirstDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerFirstDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DockerFirstDemo/DockerFirstDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DockerFirstDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
